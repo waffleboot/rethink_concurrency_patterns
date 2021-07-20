d1604ce43985867f6177f9c67e374f915023b790
Andrei Yangabishev <waffleboot@gmail.com>
2021-07-20 22:18:06 +0300

    использовал семафоры

--- main.go
+++ main.go
@@ -1,55 +1,46 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
-	"sync"
 )
 
+type token struct{}
+
 type Pool struct {
-	mu              sync.Mutex
-	cond            sync.Cond
-	numConns, limit int
-	idle            []net.Conn
+	sem  chan token
+	idle chan net.Conn
 }
 
 func NewPool(limit int) *Pool {
-	p := &Pool{limit: limit}
-	p.cond.L = &p.mu
-	return p
+	sem := make(chan token, limit)
+	idle := make(chan net.Conn, limit)
+	return &Pool{sem, idle}
 }
 
 func (p *Pool) Release(c net.Conn) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.idle = append(p.idle, c)
-	p.cond.Signal()
+	p.idle <- c
 }
 
 // hijack это типа мы крадем соединение, т.е. оно перестает учитываться в лимитах
 func (p *Pool) Hijack(c net.Conn) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.numConns--
-	p.cond.Signal()
+	<-p.sem
 }
 
-func (p *Pool) Acquire() (net.Conn, error) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	for len(p.idle) == 0 && p.numConns >= p.limit {
-		p.cond.Wait()
+func (p *Pool) Acquire(ctx context.Context) (net.Conn, error) {
+	select {
+	case conn := <-p.idle:
+		return conn, nil
+	case p.sem <- token{}:
+		conn, err := dial()
+		if err != nil {
+			<-p.sem
+		}
+		return conn, err
+	case <-ctx.Done():
+		return nil, ctx.Err()
 	}
-	if len(p.idle) > 0 {
-		c := p.idle[len(p.idle)-1]
-		p.idle = p.idle[:len(p.idle)-1]
-		return c, nil
-	}
-	c, err := dial()
-	if err == nil {
-		p.numConns++
-	}
-	return c, err
 }
 
 func dial() (net.Conn, error) {
@@ -62,7 +53,7 @@ func dial() (net.Conn, error) {
 
 func main() {
 	p := NewPool(3)
-	c, err := p.Acquire()
+	c, err := p.Acquire(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
