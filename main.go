c85df99ece75349f8e6a3067ba26d39157fb4fcb
Andrei Yangabishev <waffleboot@gmail.com>
2021-07-20 22:25:25 +0300

    queue

--- main.go
+++ main.go
@@ -59,3 +59,39 @@ func main() {
 	}
 	p.Release(c)
 }
+
+type Item struct{}
+
+type Queue struct {
+	items chan []Item
+	empty chan bool
+}
+
+func NewQueue() *Queue {
+	items := make(chan []Item, 1)
+	empty := make(chan bool, 1)
+	empty <- true
+	return &Queue{items, empty}
+}
+
+func (q *Queue) Get() Item {
+	items := <-q.items
+	item := items[0]
+	items = items[1:]
+	if len(items) == 0 {
+		q.empty <- true
+	} else {
+		q.items <- items
+	}
+	return item
+}
+
+func (q *Queue) Put(item Item) {
+	var items []Item
+	select {
+	case items = <-q.items:
+	case <-q.empty:
+	}
+	items = append(items, item)
+	q.items <- items
+}
